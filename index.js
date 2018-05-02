5c07a3797b4884474f5111c34c3790b30c5bdb1d
Aldrin Lim <aldrinlim81@gmail.com>
2018-05-02 14:51:17 +0800

    add button click

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const puppeteer = require('puppeteer');
-
+const { url, username, password } = require('./config.json');
 async function getPic() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ launch: "headless" });
   const page = await browser.newPage();
-  await page.goto('https://fb.com');
+  await page.goto(url);
   await page.setViewport({width: 2000, height: 500})
+  await page.type('#txtUsername', username );
+  await page.type('#txtPassword', password );
+  await page.click('#btnLogIn');
+  
+  await page.waitFor(15000);
+  
   await page.screenshot({path: 'out.png'});
-
+  
   await browser.close();
 
   console.log("done")
